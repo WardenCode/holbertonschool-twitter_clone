5be616311b3218a3a433aa14bd2cec561ac08e91
Diego Linares <diegojeanluck@hotmail.com>
2023-08-06 15:32:03 -0500

    1. Provider

--- twitter/lib/main.dart
+++ twitter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:twitter/providers/share_state.dart';
 import 'package:twitter/screens/signin_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          elevation: 0.0,
-          iconTheme: IconThemeData(
-            color: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SharedState()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            elevation: 0.0,
+            iconTheme: IconThemeData(
+              color: Colors.blue,
+            ),
           ),
         ),
+        home: const SignIn(),
       ),
-      home: const SignIn(),
     );
   }
 }
