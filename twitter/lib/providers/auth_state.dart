39fd02d7b3e7816d4255e2f53b1a0e3c7cb433f9
Diego Linares <diegojeanluck@hotmail.com>
2023-08-10 02:23:21 -0500

    2. Firebase

--- twitter/lib/providers/auth_state.dart
+++ twitter/lib/providers/auth_state.dart
@@ -1,3 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as f;
 import 'package:flutter/material.dart';
+import 'package:twitter/models/user.dart';
 
-class Auth extends ChangeNotifier {}
+enum Errors {
+  none,
+  matchError,
+  weakError,
+  existsError,
+  error,
+  wrongError,
+  noUserError
+}
+
+class Auth extends ChangeNotifier {
+  late f.FirebaseAuth auth = f.FirebaseAuth.instance;
+
+  final usersRef =
+      FirebaseFirestore.instance.collection('users').withConverter<User>(
+            fromFirestore: (snapshot, _) {
+              return User.fromJson(
+                snapshot.data() ?? {},
+              );
+            },
+            toFirestore: (user, _) => user.toJson(),
+          );
+
+  attemptSignUp(email, name, password, passwordConfirmation) async {
+    if (password != passwordConfirmation) {
+      return Errors.matchError;
+    }
+
+    try {
+      f.UserCredential userCredential =
+          await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return usersRef
+          .add(User(
+            userID: userCredential.user!.uid,
+            email: email,
+            userName: '@${name}Holberton',
+            displayName: name,
+          ))
+          .then((value) => Errors.none)
+          .catchError((error) => Errors.error);
+    } on f.FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return Errors.weakError;
+      } else if (e.code == 'email-already-in-use') {
+        return Errors.existsError;
+      } else {
+        return Errors.error;
+      }
+    }
+  }
+}
